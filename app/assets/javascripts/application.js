da542dc5a0eea7d61b00138d36ba513705508e09
arthurtruong <arthurtruong.work@gmail.com>
2018-05-29 12:10:00 +1000

    Added devise and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
